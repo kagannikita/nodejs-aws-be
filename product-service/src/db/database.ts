370332e11cad09a60a90ef148a71e14f089f2188
nkagan <nkagan@dar.io>
2022-07-26 17:46:13 +0600

    fix database

--- product-service/src/db/database.ts
+++ product-service/src/db/database.ts
@@ -2,7 +2,7 @@ const ServerlessClient = require('serverless-postgres')
 
 export const client = new ServerlessClient({
     user: "postgres",
-    host: "db-1.crhrmvpcwyde.us-east-1.rds.amazonaws.com",
+    host: "database-1.crhrmvpcwyde.us-east-1.rds.amazonaws.com",
     database: "lesson4",
     password: "JFjUsdgmPbaTKtQp0Dvq",
     port: "5432",
